f46d9038bac5fdf8735f146da17701fa408e0cfe
Gianluca Coco <cocogian@hotmail.com>
2022-07-02 04:07:36 +0200

    First Push

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:bmi/widget/button_widget.dart';
+import 'package:bmi/widget/navigation_drawer_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:bmi/widget/button_widget.dart';
+import 'package:bmi/widget/navigation_drawer_widget.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Welcome';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: MainPage(),
+  );
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    drawer: NavigationDrawerWidget(),
+    // endDrawer: NavigationDrawerWidget(),
+    appBar: AppBar(
+      title: Text(MyApp.title),
+    ),
+    body: Builder(
+      builder: (context) => Container(
+        alignment: Alignment.bottomRight,
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: ButtonWidget(
+          icon: Icons.open_in_new,
+          text: 'Open Drawer',
+          onClicked: () {
+            Scaffold.of(context).openDrawer();
+            // Scaffold.of(context).openEndDrawer();
+          },
+        ),
+      ),
+    ),
+  );
+}
+
+
+
+
---
+++ lib/page/rating_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RatingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text('Rating'),
+      centerTitle: true,
+      backgroundColor: Colors.deepPurple,
+    ),
+  );
+}
